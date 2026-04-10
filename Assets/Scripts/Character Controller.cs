25a6920b08b3d452683de80238d2342db5aa0238
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2026-04-10 19:01:57 +1200

    Interaction + non-moving jump animation

--- Assets/Scripts/Character Controller.cs
+++ Assets/Scripts/Character Controller.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using Unity.Android.Gradle.Manifest;
 using Unity.Cinemachine;
 using Unity.ProjectAuditor.Editor.Core;
@@ -8,42 +9,51 @@ using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour {
     // The actions defined in the InputSystemAsset relevent to this controller
-    InputAction moveAction, sprintAction, jumpAction;
+    InputAction moveAction, sprintAction, jumpAction, interactAction;
 
     [Header("Character Settings")]
     // horizontal
-    public Vector2 velocity;
-    public float walkSpeed = 1f;
-    public float sprintSpeed = 2f;
-    float maxSpeed = 0f;
-    public float acceleration = 2f;
-    public float friction = 2f;
-    float epsilon = 0.2f;
-    int xFlip = 1;
-    float scale = 0.1f;
+    float xVelocity;
+    [Tooltip("Speed for base walk")] public float walkSpeed = 1f;
+    [Tooltip("Speed for shift run")] public float runSpeed = 2f;
+    float maxSpeed = 0f; // The current max walk speed
+    [Tooltip("Rate of speed up for walking")] public float acceleration = 2f;
+    [Tooltip("Rate of stopping for walking")] public float friction = 2f;
+    float epsilon = 0.2f; // tolerance value
+    int xFlip = 1; // look direction multiplier
+    float scale; // character scale
 
     // vertical
-    public float jumpheight = 1f;
-    public float gravity = 9.81f;
-    public bool isGrounded = false;
-    bool pastGrounded = false;
-    public float coyoteTime = 0.15f;
+    [Tooltip("How high the player jumps (depends on gravity)")] public float jumpheight = 1f;
+    [Tooltip("How fast the player falls")] public float gravity = 9.81f;
+    bool isGrounded = false; // is currently on ground
+    bool pastGrounded = false; // was last frame on ground
+    [Tooltip("How close the player needs to be from the ground to count as grounded")]
+    public float groundedDistance = 0.1f;
 
+    [Header("Other")]
     // looping
     GameObject leftSide, rightSide;
-    float mapSize;
+    float mapSize; // calculated at runtime
 
     // private references
     Rigidbody2D rb;
+    BoxCollider2D col;
     Animator amr;
     Transform tf;
     CinemachineCamera cine;
+    List<GameObject> triggerOverlaps = new List<GameObject>();
 
     // Awake is called when script is initialized
     void Awake() {
         rb = GetComponent<Rigidbody2D>(); 
         amr = GetComponent<Animator>();
         tf = GetComponent<Transform>();
+        col = GetComponent<BoxCollider2D>();
+
+        // Check for missing - if so force error
+        if (rb == null || amr == null || tf == null || col == null)
+            Debug.LogWarning("Failed to setup all components in " + gameObject.name);
     } 
 
     // Start called before first frame of game, used to initialize values
@@ -52,6 +62,7 @@ public class CharacterController : MonoBehaviour {
         moveAction = InputSystem.actions.FindAction("Move");
         sprintAction = InputSystem.actions.FindAction("Sprint");
         jumpAction = InputSystem.actions.FindAction("Jump");
+        interactAction = InputSystem.actions.FindAction("Interact");
 
         // Search the scene for objects
         leftSide = GameObject.FindGameObjectWithTag("left tp");
@@ -60,26 +71,29 @@ public class CharacterController : MonoBehaviour {
 
         // Check for missing - if so force error
         if (leftSide == null || rightSide == null || cine == null)
-            Debug.LogWarning("No valid left|right teleporter");
+            Debug.LogWarning("Failed to find core GameObjects in scene");
 
         // Calculate constants
         mapSize = rightSide.transform.position.x - leftSide.transform.position.x;
+        scale = tf.localScale.x;
     }
 
     // FixedUpdate called at a fixed framerate (typically 60fps) for smoothed physics
     void FixedUpdate() {
         // Collect inputs
         Vector2 moveVector = moveAction.ReadValue<Vector2>();
-        bool jumpInput = jumpAction.IsPressed() && isGrounded;
+        bool jumpInput = jumpAction.IsPressed();
         bool sprintInput = sprintAction.IsPressed();
+        bool interactInput = interactAction.IsPressed();
 
         // Execute inputs
         SprintCharacter(sprintInput);
         MoveCharacter(moveVector);
         JumpCharacter(jumpInput);
+        Interact(interactInput);
 
         // Apply movement changes
-        transform.Translate(velocity * Time.deltaTime);
+        transform.Translate(new Vector2(xVelocity, 0) * Time.deltaTime);
         transform.localScale = new Vector3(xFlip*scale, scale); // flip the char to move dir
 
         // Run final checks
@@ -89,35 +103,51 @@ public class CharacterController : MonoBehaviour {
 
     void SprintCharacter(bool input) {
         if (input)
-            maxSpeed = sprintSpeed;
+            maxSpeed = runSpeed;
         else
             maxSpeed = walkSpeed;
     }
     void MoveCharacter(Vector2 input) {
         Vector2 delta = input.normalized * acceleration;
         if (Mathf.Abs(delta.x) <= epsilon)
-            velocity.x = Mathf.MoveTowards(velocity.x, 0, friction);
+            xVelocity = Mathf.MoveTowards(xVelocity, 0, friction);
 
-        velocity.x = Mathf.Clamp(velocity.x += delta.x, -maxSpeed, maxSpeed);
+        xVelocity = Mathf.Clamp(xVelocity += delta.x, -maxSpeed, maxSpeed);
 
-        if (velocity.x < 0) xFlip = -1;
-        else if (velocity.x > 0) xFlip = 1;
+        if (xVelocity < 0) xFlip = -1;
+        else if (xVelocity > 0) xFlip = 1;
     }
     void JumpCharacter(bool input) {
         // async event handler for coyote time
+        Vector3 startpos = new Vector2(tf.position.x, tf.position.y - (col.size.y * tf.localScale.y / 2) -0.01f);
+        RaycastHit2D rch = Physics2D.Raycast(startpos, Vector2.down, groundedDistance);
+        Debug.DrawRay(startpos, Vector2.down * groundedDistance, rch ? Color.green : Color.red, 0.05f);
+        if (rch && rch.collider.tag.Equals("ground") && rch.collider != col)
+            isGrounded = true;
+        else 
+            isGrounded = false;
 
-
-        // set gravity
-        Physics2D.gravity = new Vector2(0, -gravity);
+                // set gravity
+                Physics2D.gravity = new Vector2(0, -gravity);
 
         // handle input
-        if (input) {
+        if (input && isGrounded) {
             rb.AddForce(new Vector2(0, jumpheight), ForceMode2D.Impulse);
             amr.SetTrigger("Jumping");
         }
     }
+    void Interact(bool input) {
+        if (input && triggerOverlaps.Count > 0) {
+            foreach (GameObject t in triggerOverlaps) {
+                Interactable i;
+                if (t.TryGetComponent<Interactable>(out i)) {
+                    i.Interact();
+                }
+            }
+        }
+    }
     void Animations() {
-        amr.SetFloat("MoveSpeed", Mathf.Abs(velocity.x));
+        amr.SetFloat("MoveSpeed", Mathf.Abs(xVelocity));
         if (isGrounded && !pastGrounded) {
             amr.SetTrigger("Landing");
         }
@@ -125,27 +155,25 @@ public class CharacterController : MonoBehaviour {
         pastGrounded = isGrounded; // Set up for next update
     }
     void Teleport() {
-        if (tf.position.x < leftSide.transform.position.x && velocity.x < 0) {
+        if (tf.position.x < leftSide.transform.position.x && xVelocity < 0) {
             tf.Translate(Vector3.right * mapSize);
             TranslateCinemachinePos(Vector3.right * mapSize);
         }
-        else if (tf.position.x > rightSide.transform.position.x && velocity.x > 0) {
+        else if (tf.position.x > rightSide.transform.position.x && xVelocity > 0) {
             tf.Translate(Vector3.left * mapSize);
             TranslateCinemachinePos(Vector3.left * mapSize);
         }
     }
 
-    // Event Methods
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "ground")
-            isGrounded = true;
+    // Event methods
+    private void OnTriggerEnter2D(Collider2D trigger) {
+        triggerOverlaps.Add(trigger.gameObject);
     }
-    private void OnTriggerExit2D(Collider2D other) {
-        if (other.tag == "ground") 
-            isGrounded = false;
+    private void OnTriggerExit2D(Collider2D trigger) {
+        triggerOverlaps.Remove(trigger.gameObject);
     }
 
-    // Method helps translate the cinemachine camera (can't edit transform like normal objs)
+    // Method helps translate the cinemachine camera (can't edit cinemachine transform like normal objs)
     void TranslateCinemachinePos(Vector3 translation) {
         Vector3 pos = cine.gameObject.transform.position + translation;
         Quaternion quart = cine.gameObject.transform.rotation;
---
+++ Assets/Scripts/Interactables/Test Interactable.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class TestInteractable : MonoBehaviour, Interactable
+{
+    [SerializeField] string debugMessage = "Test Interactable was interacted with";
+    [SerializeField] bool destroyOnInteract = true;
+    bool makedForDestruction = false;
+
+    // check and force add a trigger to this object on awake
+    void Awake() {
+        Collider2D col;
+        if (!TryGetComponent<Collider2D>(out col)) // if failed to get existing collider, add one
+            col = gameObject.AddComponent<BoxCollider2D>();
+        col.isTrigger = true;
+    }
+    // last call before end of frame, helps avoid referencing errors if destruction happens here
+    void LateUpdate() {
+        if (makedForDestruction) 
+            Destroy(gameObject);
+    }
+    public void Interact() {
+        Debug.Log(debugMessage);
+        if (destroyOnInteract) 
+            makedForDestruction = true;
+    }
+}
